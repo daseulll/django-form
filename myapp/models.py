d68097bb62b2fc6b4e224f8ad9702e4141df7e06
daseul <nldaseul@gmail.com>
2019-04-16 21:37:50 +0900

    Add Comment model

--- myapp/models.py
+++ myapp/models.py
@@ -8,3 +8,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Comment(models.Model):
+    author = models.CharField(max_length=50)
+    message = models.TextField()
+    ip = models.CharField(max_length=15)
