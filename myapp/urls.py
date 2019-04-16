ee92136fdd7d27b9c00912f067c9270e9ec74e80
daseul <nldaseul@gmail.com>
2019-04-16 21:35:46 +0900

    Add post_edit feature

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('new/', views.post_new, name='post_new'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -21,3 +21,24 @@ def post_new(request):
     return render(request, template_name, {
         'form':form,
     })
+
+def post_edit(request, pk):
+    form_cls = PostForm
+    template_name = 'myapp/post_form.html'
+    success_url = '/'
+
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST': 
+        form = form_cls(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            # post = Post.objects.create(**form.cleaned_data)
+            post = form.save()
+            return redirect(success_url)
+    else:
+        form = form_cls(instance=post) 
+        
+    return render(request, template_name, {
+        'form':form,
+    })
+
