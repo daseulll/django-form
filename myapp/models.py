6f9efa505003f14ebc4c8dc0b32dfec1e1246126
daseul <nldaseul@gmail.com>
2019-04-16 21:33:37 +0900

    Add post_new feature

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import CommentForm, PostForm
+from .models import Comment, Post 
 
-# Create your views here.
+
+def post_new(request):
+    form_cls = PostForm
+    template_name = 'myapp/post_form.html'
+    success_url = '/'
+
+    if request.method == 'POST': 
+        form = form_cls(request.POST, request.FILES)
+        if form.is_valid():
+            # post = Post.objects.create(**form.cleaned_data)
+            post = form.save()
+            return redirect(success_url)
+    else:
+        form = form_cls() 
+        
+    return render(request, template_name, {
+        'form':form,
+    })
