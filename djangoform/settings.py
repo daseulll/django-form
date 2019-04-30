28c552df0ce8d1f314ff9cfbcba8e11b9aa829da
daseul <nldaseul@gmail.com>
2019-04-30 18:05:47 +0900

    Set django-summernote

--- djangoform/settings.py
+++ djangoform/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_summernote',
+
     'myapp',
+    'accounts',
+    
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
