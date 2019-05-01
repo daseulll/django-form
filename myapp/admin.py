6a5484c880db55368dbe956f9fd7278a2b197e96
daseul <nldaseul@gmail.com>
2019-05-01 20:05:00 +0900

    Customize HTML form renderer

--- myapp/admin.py
+++ myapp/admin.py
@@ -6,5 +6,6 @@ from .models import Post, Comment
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('__all__')
+    readonly_fields = ['user_agent']
 
 admin.site.register(Comment)
