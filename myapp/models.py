e83dc42be5810ed58fd24b660ecddffb3c90ba93
daseul <nldaseul@gmail.com>
2019-05-01 16:29:05 +0900

    Add User_agent field in myapp

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,7 @@ from django.core.validators import MinLengthValidator
 class Post(models.Model):
     title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
     content = models.TextField()
+    user_agent = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
